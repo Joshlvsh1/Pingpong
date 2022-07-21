b8453d551c79297c0100aa30e8ca79a8260e8dd2
Josh <joshuae16@icloud.com>
2022-07-21 12:10:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+from turtle import _Screen
+
+wn: _Screen = turtle.Screen()
+wn.title("Pong by @TokyoEdTech")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+wn.exitonclick()
+
+
+
+
+
+#
+
+
+
+
+
+
